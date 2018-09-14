84666df0badbf15f40b4f2768cc9637949f9e322
Luwei Zhang <luweizhang@luwei-peerspace.local>
2018-09-14 09:24:07 -0700

    add initial projects

---
+++ body-parts/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to body-parts
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ body-parts/test/body_parts/core_test.clj
@@ -0,0 +1,7 @@
+(ns body-parts.core-test
+  (:require [clojure.test :refer :all]
+            [body-parts.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
