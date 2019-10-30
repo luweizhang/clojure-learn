3aa398b00c17a7ddfd921b8bf284ace12a4885db
luweizhang <luwzhang@gmail.com>
2019-10-30 11:42:01 -0700

    Update 135_infix_calculator.clj

--- 4clojure/other_solutions/135_infix_calculator.clj
+++ 4clojure/other_solutions/135_infix_calculator.clj
@@ -1,4 +1,13 @@
+;; http://www.4clojure.com/problem/135
+
+;; reduce takes two inputs, the accumulated output so far, and a vector of new inputs
+;; basically, you take the first arg, and partition the rest of the args, and access them with the [op b]
+
 (fn infix [& args]
   (reduce (fn [a [op b]] (op a b))
           (first args)
           (partition 2 (rest args))))
+
+
+;; e.g. (infix 1 + 7 - 3 + 5) returns 10
+
