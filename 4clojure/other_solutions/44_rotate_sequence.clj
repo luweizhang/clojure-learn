a02c5082eced84ff31835f92f2bc186978be7338
luweizhang <luwzhang@gmail.com>
2019-11-05 11:49:22 -0800

    Update 44_rotate_sequence.clj

--- 4clojure/other_solutions/44_rotate_sequence.clj
+++ 4clojure/other_solutions/44_rotate_sequence.clj
@@ -1,3 +1,10 @@
+;; solution 1
 (fn [n seq]
   (let [k (mod n (count seq))]
     (concat (drop k seq) (take k seq))))
+
+;;solution 2
+(fn [n x]
+  (take (count x)
+        (drop (mod n (count x))
+              (cycle x))))
