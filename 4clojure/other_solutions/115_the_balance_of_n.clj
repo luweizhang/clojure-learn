8030d86fcdd121a972afc0ca201a615bd854ef14
luweizhang <luwzhang@gmail.com>
2019-11-12 22:11:18 -0800

    Update 115_the_balance_of_n.clj

--- 4clojure/other_solutions/115_the_balance_of_n.clj
+++ 4clojure/other_solutions/115_the_balance_of_n.clj
@@ -1,12 +1,10 @@
 ;; solution 1
 
 (fn [num]
-  (let [
-        vec  (vec (map int (seq (str num))))
+  (let [vec  (vec (map int (seq (str num))))
         len (count vec)
         end1  (int (/ len 2))
         start2 (if (odd? len) (+ end1 1) end1)
         side1 (subvec vec 0 end1)
-        side2 (subvec vec start2 len)
-        ]
+        side2 (subvec vec start2 len)]
     (= (apply + side1) (apply + side2))))
