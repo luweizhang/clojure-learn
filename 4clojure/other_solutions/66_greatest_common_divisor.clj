66a9c94eb58e904d142b057632fcd2c561f21ce5
luweizhang <luwzhang@gmail.com>
2019-11-13 17:22:23 -0800

    Update 66_greatest_common_divisor.clj

--- 4clojure/other_solutions/66_greatest_common_divisor.clj
+++ 4clojure/other_solutions/66_greatest_common_divisor.clj
@@ -13,3 +13,9 @@
         b-divisor (get-divisor b)
         commom-divisor (clojure.set/intersection a-divisor b-divisor)]
     (apply max commom-divisor)))
+
+;; solution 2
+
+;; similar to solution one, but you filter the range backwards and find the first
+
+(fn [a b] (first (filter #(and (zero? (mod a %)) (zero? (mod b %))) (range a 0 -1))))
